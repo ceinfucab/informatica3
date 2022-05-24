a5894929c5987c00093eb3463b8c172792060f46
Ceinf Ucab Guayana <101413827+ceinfucab@users.noreply.github.com>
2022-05-23 22:40:44 -0400

    enunciado
    enunciado

--- Arquitecturas/src/newpackage/Main.java
+++ Arquitecturas/src/newpackage/Main.java
@@ -1,6 +1,7 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ En un Taller de Proyectos de Arquitectura, existen 10 Jefes donde se desea saber su codigo, nombre, direccion y telefono  y ellos asignan Proyectos a arquitectos,  del cual se les desea conocer su codigo, nombre, direccion y telefono y el código del plano que se le es asignado para realizar un proyecto. Cada arquitecto tiene a 10 albañiles trabajadores, del cual también se les desea conocer su código, nombre, dirección y teléfono y la fecha de asignación del plano para su proyecto asignado. También se desea calcular el área y el perimetro de 3 tipos de figuras, las cuales son, rectangulos, cuadrados y 3 tipos de triangulos. Realice un programa en Java que resuelva este problema, usando POO y Polimorfismo, sobre escritura de métodos e implementaciones que crea necesarias, mostrando los datos en pantalla.
  */
 package newpackage;
 
